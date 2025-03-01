a4645e89874b3a06d00aa306ddc122b550150680
Aoew1518 <1518703146@qq.com>
2025-03-01 18:59:50 +0800

    更改配置依赖

--- react-chatapp/util/http.ts
+++ react-chatapp/util/http.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const httpInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_LOGIN_BASE_URL,
+    // baseURL: process.env.NEXT_PUBLIC_LOGIN_BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
